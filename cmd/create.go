d272a7bb3d3922ddb00dbe8bf159471bc432e91f
Shota Sawada <shota.sawada@crowdworks.co.jp>
2018-09-20 12:01:49 +0900

    refactor: move code yaml read function

--- cmd/create.go
+++ cmd/create.go
@@ -2,27 +2,16 @@ package cmd
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
-	"os"
 	"os/exec"
-	"path"
 	"regexp"
-	"strings"
+
+	"github.com/sawadashota/gh-issue/issueyaml"
 
 	"github.com/sawadashota/gh-issue"
 	"github.com/sawadashota/gh-issue/eloquent"
 	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v2"
 )
-
-type issueYaml struct {
-	input string
-	abs   string
-	body  map[string]interface{}
-}
-
-var i *issueYaml
 
 var Create = &cobra.Command{
 	Use:   "create -f [filepath]",
@@ -32,47 +21,32 @@ var Create = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		token, err := getToken()
 		if err != nil {
-			log.Fatal(err)
+			eloquent.NewError(err.Error()).Exec()
 		}
 
-		createIssues(file, token)
+		if err := createIssues(file, token); err != nil {
+			eloquent.NewError(err.Error()).Exec()
+		}
 	},
 }
 
 func createIssues(fp, token string) error {
-	i = newYaml(fp)
-	if !i.isYamlExtension() {
-		return fmt.Errorf("file extension should be issueYaml or yml")
-	}
-
-	meta := i.body["meta"].(interface{})
-	repo, err := getString(meta, "repo")
-	if err != nil {
-		return err
-	}
-	o, r, err := splitOwnerRepo(repo)
+	y, err := issueyaml.New(fp)
 
 	if err != nil {
 		return err
 	}
 
-	issues := issues(o, r, token, i.body["issues"].([]interface{}))
+	issues, err := y.Issues(token)
+	if err != nil {
+		return err
+	}
+
 	results := issues.Create()
 
 	stdoutAllError(results)
 
 	return nil
-}
-
-func splitOwnerRepo(repo string) (owner string, name string, err error) {
-	arr := strings.Split(repo, "/")
-	if len(arr) != 2 {
-		return "", "", fmt.Errorf("invalid repository name: %s", repo)
-	}
-
-	owner = arr[0]
-	name = arr[1]
-	return
 }
 
 func stdoutAllError(results *[]ghissue.Result) {
@@ -90,109 +64,6 @@ func stdoutAllError(results *[]ghissue.Result) {
 	}
 
 	eloquent.NewError("%d errors occurred.\n", errCount).Important().Exec()
-}
-
-// Yamlのissues以下を受け取り、構造体を返す
-func issues(owner string, repo string, token string, yaml []interface{}) *ghissue.Issues {
-	issues := ghissue.New(owner, repo, token)
-	for _, issue := range yaml {
-		var ops []ghissue.Option
-
-		title, err := getString(issue, "title")
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		assignee, err := getString(issue, "assignee")
-		if err == nil {
-			ops = append(ops, ghissue.WithAssignee(assignee))
-		}
-
-		body, err := getString(issue, "body")
-		if err == nil {
-			ops = append(ops, ghissue.WithBody(body))
-		}
-
-		labels, err := getSlice(issue, "labels")
-
-		if err == nil {
-			for _, label := range labels {
-				ops = append(ops, ghissue.WithLabel(label))
-			}
-		}
-
-		issues.AddIssue(title, ops...)
-	}
-
-	return issues
-}
-
-// Yamlから値がstringの値を取り出す
-func getString(yaml interface{}, key string) (string, error) {
-	for k, v := range yaml.(map[interface{}]interface{}) {
-		if fmt.Sprintf("%v", k) == key {
-			return fmt.Sprintf("%v", v), nil
-		}
-	}
-
-	return "", fmt.Errorf("key: \"%v\" is not exist in yaml file", key)
-}
-
-// Yamlから値が[]stringの値を取り出す
-func getSlice(yaml interface{}, key string) ([]string, error) {
-	var slice []string
-	for k, v := range yaml.(map[interface{}]interface{}) {
-		if fmt.Sprintf("%v", k) == key {
-			for _, label := range v.([]interface{}) {
-				slice = append(slice, fmt.Sprintf("%v", label))
-			}
-			return slice, nil
-		}
-	}
-
-	return slice, fmt.Errorf("key: %v is not exist", key)
-}
-
-// Set absolution path for issueYaml file
-func newYaml(fp string) *issueYaml {
-	i := &issueYaml{input: fp}
-
-	pwd, err := os.Getwd()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if strings.HasPrefix(fp, "/") {
-		i.abs = fp
-	} else {
-		i.abs = path.Join(pwd, i.input)
-	}
-	i.read()
-
-	return i
-}
-
-func (i *issueYaml) read() {
-	buf, err := ioutil.ReadFile(i.abs)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	m := make(map[string]interface{})
-	err = yaml.Unmarshal(buf, &m)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	i.body = m
-}
-
-func (i *issueYaml) isYamlExtension() bool {
-	r := regexp.MustCompile(`\.(yaml|yml)$`)
-	return r.MatchString(i.abs)
 }
 
 func contains(s interface{}, e string) bool {
--- issueyaml/issueyaml.go
+++ issueyaml/issueyaml.go
@@ -2,41 +2,167 @@ package issueyaml
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
+	"path"
+	"regexp"
+	"strings"
+
+	"github.com/sawadashota/gh-issue"
+	"gopkg.in/yaml.v2"
 )
 
-const (
-	FileName        = "issues.yml"
-	DefaultTemplate = `# template
-meta:
-  repo: owner/reponame
-
-issues:
-  - title: issue title 1
-    assignee: assignee
-    body: ""
-    labels:
-      - enhancement`
-)
-
-// Create template yaml
-func Create(path, template string) error {
-	if exists(path) {
-		return fmt.Errorf("%v is already exists.\n", path)
-	}
-
-	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0666)
-	defer f.Close()
-
-	if err != nil {
-		return err
-	}
-
-	fmt.Fprintln(f, template)
-	return nil
+type Yaml struct {
+	input string
+	abs   string
+	body  map[string]interface{}
 }
 
-func exists(filepath string) bool {
-	_, err := os.Stat(filepath)
-	return err == nil
+var yamlExtRegexp *regexp.Regexp
+
+func init() {
+	yamlExtRegexp = regexp.MustCompile(`\.(yaml|yml)$`)
+}
+
+func (y *Yaml) OwnerRepo() (owner string, name string, err error) {
+	meta := y.body["meta"].(interface{})
+	repo, err := getString(meta, "repo")
+	if err != nil {
+		return "", "", err
+	}
+
+	r, o, err := splitOwnerRepo(repo)
+	if err != nil {
+		return "", "", err
+	}
+
+	return o, r, nil
+}
+
+// Yamlのissues以下を受け取り、構造体を返す
+func (y *Yaml) Issues(token string) (*ghissue.Issues, error) {
+	repo, owner, err := y.OwnerRepo()
+	if err != nil {
+		return nil, err
+	}
+
+	issues := ghissue.New(owner, repo, token)
+	for _, issue := range y.issues() {
+		var ops []ghissue.Option
+
+		title, err := getString(issue, "title")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		assignee, err := getString(issue, "assignee")
+		if err == nil {
+			ops = append(ops, ghissue.WithAssignee(assignee))
+		}
+
+		body, err := getString(issue, "body")
+		if err == nil {
+			ops = append(ops, ghissue.WithBody(body))
+		}
+
+		labels, err := getSlice(issue, "labels")
+
+		if err == nil {
+			for _, label := range labels {
+				ops = append(ops, ghissue.WithLabel(label))
+			}
+		}
+
+		issues.AddIssue(title, ops...)
+	}
+
+	return issues, nil
+}
+
+// Set absolution path for issueYaml file
+func New(fp string) (*Yaml, error) {
+	if !isYamlExtension(fp) {
+		return nil, fmt.Errorf("file extension should be issueYaml or yml")
+	}
+
+	i := &Yaml{input: fp}
+
+	pwd, err := os.Getwd()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if strings.HasPrefix(fp, "/") {
+		i.abs = fp
+	} else {
+		i.abs = path.Join(pwd, i.input)
+	}
+	i.read()
+
+	return i, nil
+}
+
+func (y *Yaml) read() {
+	buf, err := ioutil.ReadFile(y.abs)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m := make(map[string]interface{})
+	err = yaml.Unmarshal(buf, &m)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	y.body = m
+}
+
+func (y *Yaml) issues() []interface{} {
+	return y.body["issues"].([]interface{})
+}
+
+// yamlの拡張子かどうか
+func isYamlExtension(fp string) bool {
+	return yamlExtRegexp.MatchString(fp)
+}
+
+// Yamlから値がstringの値を取り出す
+func getString(yaml interface{}, key string) (string, error) {
+	for k, v := range yaml.(map[interface{}]interface{}) {
+		if fmt.Sprintf("%v", k) == key {
+			return fmt.Sprintf("%v", v), nil
+		}
+	}
+
+	return "", fmt.Errorf("key: \"%v\" is not exist in yaml file", key)
+}
+
+// Yamlから値が[]stringの値を取り出す
+func getSlice(yaml interface{}, key string) ([]string, error) {
+	var slice []string
+	for k, v := range yaml.(map[interface{}]interface{}) {
+		if fmt.Sprintf("%v", k) == key {
+			for _, label := range v.([]interface{}) {
+				slice = append(slice, fmt.Sprintf("%v", label))
+			}
+			return slice, nil
+		}
+	}
+
+	return slice, fmt.Errorf("key: %v is not exist", key)
+}
+
+func splitOwnerRepo(repo string) (owner string, name string, err error) {
+	arr := strings.Split(repo, "/")
+	if len(arr) != 2 {
+		return "", "", fmt.Errorf("invalid repository name: %s", repo)
+	}
+
+	owner = arr[0]
+	name = arr[1]
+	return
 }
